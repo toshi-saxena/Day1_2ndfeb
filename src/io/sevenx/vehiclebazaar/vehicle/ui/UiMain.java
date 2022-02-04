0549390b81c50e352e0418588de3d5921a89c296
toshi-saxena <98867634+toshi-saxena@users.noreply.github.com>
2022-02-04 09:20:05 +0530

    Vechicle Bazaar after improvisation

--- src/io/sevenx/vehiclebazaar/vehicle/ui/UiMain.java
+++ src/io/sevenx/vehiclebazaar/vehicle/ui/UiMain.java
@@ -14,8 +14,7 @@ public class UiMain {
 		b.setMileage(35);
 		b.setTypeOfFuel("Petrol");
 		b.setMaximumSpeed(110);
-		b.setNumberOfGear(5);
-		b.setPower(350);
+		
 		System.out.println("The Brand of Vehicle is:"+b.getBrand()+" Model of Bike is:"+b.getModel()+"Color is:"+b.getColor());
 		System.out.println("The Mileage is"+b.getMileage()+"Maximum Speed"+b.getMaximumSpeed());
 		System.out.println("The Type of Fuel is:"+b.getTypeOfFuel());
@@ -28,20 +27,23 @@ public class UiMain {
 		c.setMileage(22);
 		c.setTypeOfFuel("Petrol");
 		c.setMaximumSpeed(145);
-		c.setHorsePower(740);
-		c.setTransmission("Automatic");
+		
 		System.out.println("The Brand of Vehicle is :"+c.getBrand()+"Model of Car is:"+c.getModel()+"Color is:"+c.getColor());
 		System.out.println("The Milage of Car is "+c.getMileage()+"Maximum Speed is:"+c.getMaximumSpeed());
 		System.out.println("The type of Fuel is:"+c.getTypeOfFuel());
 		System.out.println("----------------------------------------------");
 		if(vehicle instanceof Bike) {
 			Bike bike=(Bike)vehicle;
+			bike.setNumberOfGear(5);
+			bike.setPower(350);
 			System.out.println("Bike has "+bike.getNumberOfGear()+" gear");
 			System.out.println("Engine is "+bike.getPower()+"cc");
 			System.out.println("----------------------------------------------");
 		}
 		else if (vehicle instanceof Car) {
 			Car car=(Car)vehicle;
+			car.setHorsePower(740);
+			car.setTransmission("Automatic");
 			System.out.println("Transmission of Car is"+car.getTransmission());
 			System.out.println("Engine has"+car.getHorsePower()+"horsepower");
 			System.out.println("----------------------------------------------");
